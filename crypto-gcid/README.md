77ed0a2ee9911160a60b460784da0e5668af7c8d
Sterne Lee <sternelee@gmail.com>
2021-05-06 20:11:18 +0800

    更新,测试wasm

--- crypto-gcid/README.md
+++ crypto-gcid/README.md
@@ -5,3 +5,8 @@ cargo install wasm-gc
 cargo build --target wasm32-unknown-unknown --release
 
 wasm-gc target/wasm32-unknown-unknown/release/crypto_gcid.wasm
+
+使用 wasm-pack
+
+wasm-pack build
+wasm-pack build --release --target web
--- crypto-gcid/src/lib.rs
+++ crypto-gcid/src/lib.rs
@@ -6,16 +6,24 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern {
     pub fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: usize);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u8array(a: &[u8]);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    log(&format!("Hello, {}!", name));
 }
 
-#[no_mangle]
-#[warn(unused_comparisons)]
-pub extern "C" fn calc_block_size (filesize: usize) -> usize {
+#[wasm_bindgen]
+pub fn calc_block_size (filesize: usize) -> usize {
     if filesize >= 0 && filesize <= (128 << 20) {
         256 << 10
     } else if filesize > (128 << 20) && filesize <= (256 << 20) {
@@ -27,13 +35,23 @@ pub extern "C" fn calc_block_size (filesize: usize) -> usize {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn calculate(buffers: &[u8]) -> String {
-    let filesize = buffers.len();
+// #[wasm_bindgen]
+// pub fn str2buffer (str: String) {
+//   return str.as_bytes();
+// }
+
+#[wasm_bindgen]
+pub fn calculate(buffer: &[u8]) -> String {
+    // 不支持传送arraybuffer: https://github.com/rustwasm/wasm-bindgen/issues/1961
+    // let buffer = buf.as_bytes();
+    log_u8array(&buffer);
+    let filesize = buffer.len();
     let mut block_size = calc_block_size(filesize);
     block_size = cmp::min(filesize, block_size);
     let mut hasher = Sha1::new();
     let mut count = 1;
+    log_u32(filesize);
+    log_u32(block_size);
     let result: String = loop {
       if count > filesize {
         break format!("{:X}", hasher.finalize());
@@ -43,18 +61,15 @@ pub extern "C" fn calculate(buffers: &[u8]) -> String {
               let n = count / block_size;
               let start = block_size * (n - 1);
               let end = block_size * n;
-              hasher.update(&buffers[start..end]);
+              hasher.update(&buffer[start..end]);
               count += 1;
+              log_u32(count);
             },
             _ => count += 1,
           }
       }
     };
+
+  log(&result);
   return result;
 }
-
-#[no_mangle]
-pub extern "C" fn add_one(x: i32) -> i32 {
-    x + 1
-}
-
